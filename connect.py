f6d6d148ca77689d43dfa3b2ff67f410d96abe65
AbdullahRepo <145813580+AbdullahRepo@users.noreply.github.com>
2024-08-15 16:57:46 +0500

    This is the first comment

---
+++ connect.py
@@ -0,0 +1,12 @@
+import pyodbc
+
+cnxn_str = (
+    r'ODBC Driver 18 for SQL Server};'
+    r'Server=tcp:server-ha-ya.database.windows.net,1433;'
+    r'Database=database-ha-ya;'
+    r'Uid=UserID-ha-ya;'
+    r'Pwd=P@ssword-ha-ya'
+)
+
+conn = pyodbc.connect(cnxn_str)
+print("Connection successful")
